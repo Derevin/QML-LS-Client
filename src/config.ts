da12590c6e6d91fad613e0f5f33d8c8bd0d587a2
Vojtěch Procházka <thederevin@gmail.com>
2024-05-17 09:32:41 +0200

    Substitute ${workspaceRoot} and ${workspaceFolder} variables in config

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,36 @@
 import * as vscode from 'vscode';
 
 
-export namespace Config {
-	function getConfig() {
-		return vscode.workspace.getConfiguration('qml-ls-client');
-	}
-	
+export namespace Config {	
 	export function getQmllsPath() : string  {
-		return getConfig().get('qmllsPath') || "";
+		return substituted(getConfig().get('qmllsPath') || "");
 	}
 	
 	export function getBuildDir() : string  {
-		return getConfig().get('buildDir') || "";
+		return substituted(getConfig().get('buildDir') || "");
+	}
+	
+	function getConfig() {
+		return vscode.workspace.getConfiguration('qml-ls-client');
+	}
+
+	function substituted(preSubstitution: string) : string {
+		preSubstitution = preSubstitution.replace(/\$\{(.*?)\}/g, (match, name) => {
+			// keep as was in case no substitution available
+			return substitutionVars(name) ?? match;
+		  }) as unknown as string;
+
+		return preSubstitution;
+	}
+
+	function substitutionVars(name: string) : string|undefined  {
+		if (name === 'workspaceRoot' || name === 'workspaceFolder') {
+			if (vscode.workspace.workspaceFolders === undefined){
+				return undefined;
+			}
+
+			return vscode.workspace.workspaceFolders[0].uri.path;
+		}
+		return undefined;
 	}
 }
